32b1f0e7fabd68c9d117bd8e7e641214f2ef05e5
marinaserdyukz <serdyukmarina876@gmail.com>
2024-08-25 21:29:59 +0200

    task 5

---
+++ js/task-5.js
@@ -0,0 +1,16 @@
+const bodyElem = document.querySelector("body")
+const btnElem = document.querySelector("button.change-color")
+const spanElem = document.querySelector(".color")
+
+
+btnElem.addEventListener("click", () => {
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+
+  spanElem.textContent = `Current color: ${newColor}`
+})
